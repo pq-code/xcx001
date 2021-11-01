01839243464ff1eaf79ba296fb88c6b930499e49
pq <>
2021-11-01 20:42:11 +0800

    初始化

---
+++ 小程序02/uni-app01/pages/index/index.vue
@@ -0,0 +1,56 @@
+<template>
+	<view class="content">
+	
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+		<navigation-bar>
+			<title>1231</title>
+		</navigation-bar>
+	</view>
+	
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: '微信小程序开发'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+</style>
---
+++ 小程序03/uni-app03/pages/index/index.vue
@@ -0,0 +1,60 @@
+<template>
+	<view>
+		<view class="home-title">品牌动态</view>
+	</view>
+<!-- 	<view class="content">
+		<image class="logo" src="/static/logo.png"></image>
+		<view class="text-area">
+			<text class="title">{{title}}</text>
+		</view>
+	</view> -->
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: ''
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.logo {
+		height: 200rpx;
+		width: 200rpx;
+		margin-top: 200rpx;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 50rpx;
+	}
+
+	.text-area {
+		display: flex;
+		justify-content: center;
+	}
+
+	.title {
+		font-size: 36rpx;
+		color: #8f8f94;
+	}
+	.home-title {
+		padding: 10px;
+		font-size: 16px;
+		
+	}
+</style>
